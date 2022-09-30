510adb7eb7eaa578c9b05ecc793dff51dd4f9e67
elHongo14 <elhongo1409@outlook.com>
2022-09-30 13:32:10 -0500

    agregamos html y parte del js y json

---
+++ js/app.js
@@ -0,0 +1,31 @@
+console.log('conectau')
+const cards = document.getElementById('cards')
+const items = document.getElementById('items')
+const footer = document.getElementById('footer')
+const templateCard = document.getElementById('template-card').content
+const templateFooter = document.getElementById('template-footer').content
+const templateCarrito = document.getElementById('template-carrito').content
+const fragment = document.createDocumentFragment()
+let carrito = {}
+
+document.addEventListener('DOMContentLoaded', () => {
+    obtenerProductos()
+})
+
+    const obtenerProductos = async () => {
+        const res = await fetch('/api/productos.json')
+        const data = await res.json()
+        //console.log(data)
+        pintarCards(data)
+    }
+
+const pintarCards = (data) => {
+    data.forEach(item => {
+        templateCard.querySelector('h5').textContent = item.title
+        templateCard.querySelector('p').textContent = item.precio
+        templateCard.querySelector('button').dataset.id = item.id
+        const clone = templateCard.cloneNode(true)
+        fragment.appendChild(clone)
+    })
+    cards.appendChild(fragment)
+}
